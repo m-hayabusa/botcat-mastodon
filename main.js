082b62382b33cee3dc5af2564db9f7fb82d4ff19
hayabusa <info@hs-sh.net>
2018-01-05 10:14:10 +0900

    「あかりちゃん」のメッセージ・管理者アカウントを差し替え

--- main.js
+++ main.js
@@ -27,107 +27,30 @@ client.on('connect', function(connection) {
                 if (json['account']) {
                     let acct = json['account']['acct'];
                     let text = json['content'];
-                    if (acct !== "yuzu") {
+                    if (acct !== config.user) {
                         if (is_running) {
                             //終了
-                            if (acct === "y" || acct === "imncls" || acct === "_5" || acct === "Knzk") {
+                            if (acct === config.admin || acct === "imncls" || acct === "_5" || acct === "Knzk") {
                                 if (text.match(/!stop/i)) {
-                                    if (acct !== "y") {
-                                        post("@"+acct+" @y 終了しました。", {}, "direct");
+                                    if (acct !== config.admin) {
+                                        post("@"+acct+" 終了しました。", {}, "direct");
                                     }
-                                    post("そろおち～", {}, "public", true);
+                                    post("終了したにゃ", {}, "public", true);
                                     change_running(0);
                                     console.log("OK:STOP:@"+acct);
                                 }
                             }
 
-                            //話題感知
-                            if (text.match(/(ねじり|わさび|ねじわさ|KnzkApp|神崎丼アプリ)/i)) {
-                                post("@y ねじり検知", {in_reply_to_id: json['id']}, "direct");
-                                rt(json['id']);
-                                console.log("OK:match:"+acct);
-                            }
+                            let regStart = /(猫|ねこ|Cat)(Bot|ぼっと)、?/i
 
-                            //こおりたそと一緒にエタフォ
-                            if (text.match(/エターナルフォースブリザード/i)) {
-                                post("@"+acct+" 私も.....！！！", {in_reply_to_id: json['id']});
-                                eti = 0;
-                                etfav(json['id']);
-                                console.log("OK:エタフォ:"+acct);
-                            }
-
-                            //メイン部分
-                            if (text.match(/(a!|あかり)/i)) {
-                                rt(json['id']);
-
-                                if (text.match(/あかり \(Bot\)さん/i) && acct === "1") {
-                                    post("こおりちゃんこんにちは！");
-                                }
-
-                                if (text.match(/(URL|リンク|短縮)/i) && config.urlshort_api) {
-                                    URL(json);
-                                }
-
-                                //埋める
-                                if (text.match(/埋め(たい|ろ|て)/i)) {
-                                    let name = "", postm = 0, postd = 0;
-
-                                    if (text.match(/(神崎|おにいさん|お兄さん)/i)) {
-                                        name = "knzk";
-                                    } else {
-                                        post("@"+acct+" ごめんね、今は神埼おにいさん以外埋められないの...", {in_reply_to_id: json['id']}, "direct");
-                                    }
-                                    if (name === "knzk") {
-                                        postd = 1;
-                                        postm = 0;
-                                    }
-
-                                    if (postm) {
-                                        post(":minecraft_dirt:​:minecraft_dirt:​:minecraft_dirt:​:minecraft_dirt:​:minecraft_dirt:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:"+name+":​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:");
-                                        console.log("OK:埋める:"+acct);
-                                    } else if (postd) {
-                                        post(":minecraft_dirt:​:minecraft_dirt:​:minecraft_dirt:​:minecraft_dirt:​:minecraft_dirt:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_stone:​:minecraft_stone:​:"+name+":​:minecraft_stone:​:minecraft_stone:\n" +
-                                            ":minecraft_bedrock:​:minecraft_bedrock:​:minecraft_bedrock:​:minecraft_bedrock:​:minecraft_bedrock:", {cw: "ｺﾞｺﾞｺﾞｺﾞｺﾞｺﾞ..."});
-                                        console.log("OK:埋める(岩盤):"+acct);
-                                    }
-                                }
-
-                                //たこ焼き (ちょくだいさんに無能扱いされたので)
-                                if (text.match(/たこ(焼き|やき)/i) && text.match(/((焼|や)いて|(作|つく)って|(食|た)べたい|ちょ(ー|～|う|く)だい|(欲|ほ)しい|お(願|ねが)い)/i)) {
-                                    setTimeout(function () {
-                                        post("@"+acct+" たこ焼きど～ぞ！\n\n" +
-                                            "[large=5x]:takoyaki:[/large]");
-                                    }, 5000);
-                                    console.log("OK:takoyaki:"+acct);
-                                }
-                            }
+                             //メイン部分
+                             if (text.match(regStart)) {
+                                fav(json['id']);
+                             }
                         } else {
-                            if (acct === "y") {
-                                if (text.match(/!start/i) || text.match(/あかり(ちゃん|たそ)(起動|おきて|起きて)/i)) {
-                                    post("おはおは～");
+                            if (acct === config.admin) {
+                                if (text.match(/!start/i) || text.match(/(猫Bot|ねこぼっと)(起動|おきて|起きて)/i)) {
+                                    post("起動したにゃ");
                                     change_running(1);
                                 }
                             }
@@ -136,12 +59,10 @@ client.on('connect', function(connection) {
                 }
             }
         } catch (e) {
-            post("ごほっ、ごほっ...\n" +
-                "ちょっと体調悪いから休む...\n\n" +
-                "【エラーが発生したため停止します】");
+            post("エラーが発生したです\n止まるです");
             change_running(0);
 
-            post("@y 【エラー検知】\n\n"+ e, {}, "direct");
+            post("@"+config.admin+"  【エラー検知】\n\n"+ e, {}, "direct");
         }
     });
 });
@@ -150,91 +71,6 @@ client.connect("wss://" + config.domain + "/api/v1/streaming/?access_token=" + c
 
 
 // ここからいろいろ
-function URL(json) {
-    post("@"+json['account']['acct']+" 送信してるから数十秒まっててねー！", {in_reply_to_id: json['id']});
-    setTimeout( function() {
-        fetch("https://" + config.domain + "/api/v1/statuses/"+json['id']+"/card", {
-            method: 'GET'
-        }).then(function(response) {
-            if(response.ok) {
-                return response.json();
-            } else {
-                throw new Error();
-            }
-        }).then(function(json_url) {
-            if (json_url["url"]) {
-                fetch("https://" + config.urlshort_api + "?akari_id=Akari_"+json['account']['acct']+"&url="+encodeURIComponent(json_url["url"]), {
-                    method: 'GET'
-                }).then(function(response) {
-                    if(response.ok) {
-                        return response.text();
-                    } else {
-                        throw new Error();
-                    }
-                }).then(function(text) {
-                    if (text.match(/http/i)) {
-                        post("@"+json['account']['acct']+" はいど～ぞ！\n"+text, {in_reply_to_id: json['id']});
-                    } else {
-                        post("@"+json['account']['acct']+" @y 何か失敗したみたい... エラー:"+text, {in_reply_to_id: json['id']}, "direct");
-                        console.warn("NG:url:"+json);
-                    }
-                }).catch(function(error) {
-                    post("@"+json['account']['acct']+" @y APIにアクセスできなかった...", {in_reply_to_id: json['id']}, "direct");
-                    console.warn("NG:url:SERVER");
-                });
-            } else {
-                post("@"+json['account']['acct']+" ...？\nURLが取得できなかった...", {in_reply_to_id: json['id']});
-            }
-        }).catch(function(error) {
-            console.warn("NG:url_card:SERVER");
-        });
-    }, 20000);
-}
-
-function etfav(id) {
-    fetch("https://" + config.domain + "/api/v1/statuses/"+id+"/favourite", {
-        headers: {'content-type': 'application/json', 'Authorization': 'Bearer '+config.token},
-        method: 'POST'
-    }).then(function(response) {
-        if(response.ok) {
-            return response.json();
-        } else {
-            throw new Error();
-        }
-    }).then(function(json) {
-        if (json["id"]) {
-            console.log("OK:fav");
-            fetch("https://" + config.domain + "/api/v1/statuses/"+id+"/unfavourite", {
-                headers: {'content-type': 'application/json', 'Authorization': 'Bearer '+config.token},
-                method: 'POST'
-            }).then(function(response) {
-                if(response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error();
-                }
-            }).then(function(json) {
-                if (json["id"]) {
-                    console.log("OK:fav");
-                    if (eti < 20) {
-                        etfav(id);
-                        eti++;
-                    }
-                } else {
-                    console.warn("NG:fav:"+json);
-                }
-            }).catch(function(error) {
-                console.warn("NG:fav:SERVER");
-            });
-        } else {
-            console.warn("NG:fav:"+json);
-        }
-    }).catch(function(error) {
-        console.warn("NG:fav:SERVER");
-    });
-}
-
-
 function rt(id) {
     fetch("https://" + config.domain + "/api/v1/statuses/"+id+"/reblog", {
         headers: {'content-type': 'application/json', 'Authorization': 'Bearer '+config.token},
